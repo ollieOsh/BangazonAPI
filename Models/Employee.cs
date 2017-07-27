e67ce69cbe1e03d58c57327c834901c55836cab0
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-07-27 14:11:41 -0500

    updated EmployeeController.cs file

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -14,7 +14,7 @@ namespace BangazonAPI.Models
         [Required]
         public string EmployeePhone {get; set;}
         [Required]
-        public int DeptId {get; set;}
+        public int? DeptId {get; set;}
         public Department Department {get; set;}
         [Required]
         public bool IsSupervisor {get; set;}
